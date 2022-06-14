811bc7557b635b0bc00137d31af814fee356aa43
Ram Gopinathan <rgopinat@redhat.com>
2022-06-14 16:10:27 -0400

    roxctl installer fixes

--- src/installer.ts
+++ src/installer.ts
@@ -2,6 +2,8 @@ import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
 import * as ioUtil from "@actions/io/lib/io-util";
 import * as fs from "mz/fs";
+import * as glob from "glob";
+
 import { FindBinaryStatus } from "./helper";
 import { RHACS_ASSETS_BASE_URL } from "./constants";
 
@@ -14,25 +16,35 @@ export class Installer {
             core.debug("Error building roxctl download URL");
         }
         core.info("Downloading roxctl");
-        return Installer.download(url);
+        return Installer.download(url, runnerOS);
     }
 
-    static async download(url: string): Promise<FindBinaryStatus> {
+    static async download(url: string, runnerOS: string): Promise<FindBinaryStatus> {
         if (!url) {
             return { found: false, reason: "URL to download roxctl is not valid." };
         }
-        const roxctl = await tc.downloadTool(url);
-        if (!(await ioUtil.exists(roxctl))) {
+        let downloadDir = "";
+        const f = await tc.downloadTool(url);
+        if (runnerOS === "Windows") {
+            downloadDir = await tc.extractZip(f);
+        }
+        else {
+            downloadDir = await tc.extractTar(f);
+        }
+        let roxctlBinary: string = this.roxCtlBinaryByOS(runnerOS);
+        roxctlBinary = await Installer.findroxCtlBinary(downloadDir, roxctlBinary);
+
+        if (!(await ioUtil.exists(roxctlBinary))) {
             return {
                 found: false,
                 reason: `An error occurred while downloading roxctl from ${url}.`
-                + `File ${roxctl} not found.`,
+                + `File ${roxctlBinary} not found.`,
             };
         }
-        fs.chmodSync(roxctl, "0755");
+        fs.chmodSync(roxctlBinary, "0755");
         return {
             found: true,
-            path: roxctl,
+            path: roxctlBinary,
         };
     }
 
@@ -51,4 +63,23 @@ export class Installer {
         core.info(`Final roxctl download url: ${url}`);
         return url;
     }
+
+    private static roxCtlBinaryByOS(osType: string): string {
+        if (osType.includes("Windows")) return "roxctl.exe";
+        return "roxctl";
+    }
+
+    static async findroxCtlBinary(folder: string, file: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            glob(`${folder}/**/${file}`, (err, res) => {
+                if (err) {
+                    reject(new Error(`Unable to find s2i exewcutable inside the directory ${folder}`));
+                }
+                else {
+                    resolve(res[0]);
+                }
+            });
+        });
+    }
+
 }
