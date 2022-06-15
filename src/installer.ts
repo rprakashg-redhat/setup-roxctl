8147ad34360f7dd08f5d9f3eb567e108d6357b07
Ram Gopinathan <rgopinat@redhat.com>
2022-06-15 09:18:37 -0400

    cleaned up roxctl install logic

--- src/installer.ts
+++ src/installer.ts
@@ -2,7 +2,6 @@ import * as core from "@actions/core";
 import * as tc from "@actions/tool-cache";
 import * as ioUtil from "@actions/io/lib/io-util";
 import * as fs from "mz/fs";
-import * as glob from "glob";
 
 import { FindBinaryStatus } from "./helper";
 import { RHACS_ASSETS_BASE_URL } from "./constants";
@@ -16,24 +15,14 @@ export class Installer {
             core.debug("Error building roxctl download URL");
         }
         core.info("Downloading roxctl");
-        return Installer.download(url, runnerOS);
+        return Installer.download(url);
     }
 
-    static async download(url: string, runnerOS: string): Promise<FindBinaryStatus> {
+    static async download(url: string): Promise<FindBinaryStatus> {
         if (!url) {
             return { found: false, reason: "URL to download roxctl is not valid." };
         }
-        let downloadDir = "";
-        const f = await tc.downloadTool(url);
-        if (runnerOS === "Windows") {
-            downloadDir = await tc.extractZip(f);
-        }
-        else {
-            downloadDir = await tc.extractTar(f);
-        }
-        let roxctlBinary: string = this.roxCtlBinaryByOS(runnerOS);
-        roxctlBinary = await Installer.findroxCtlBinary(downloadDir, roxctlBinary);
-
+        const roxctlBinary = await tc.downloadTool(url);
         if (!(await ioUtil.exists(roxctlBinary))) {
             return {
                 found: false,
@@ -62,24 +51,6 @@ export class Installer {
         }
         core.info(`Final roxctl download url: ${url}`);
         return url;
-    }
-
-    private static roxCtlBinaryByOS(osType: string): string {
-        if (osType.includes("Windows")) return "roxctl.exe";
-        return "roxctl";
-    }
-
-    static async findroxCtlBinary(folder: string, file: string): Promise<string> {
-        return new Promise((resolve, reject) => {
-            glob(`${folder}/**/${file}`, (err, res) => {
-                if (err) {
-                    reject(new Error(`Unable to find s2i exewcutable inside the directory ${folder}`));
-                }
-                else {
-                    resolve(res[0]);
-                }
-            });
-        });
     }
 
 }
